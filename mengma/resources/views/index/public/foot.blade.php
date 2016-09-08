28c9fc4b11ee8aaad650c0d6a3ed59ae524a3435
刘晓康 <1351369437@qq.com>
2016-09-08 16:48:20 +0800

    *

---
+++ mengma/resources/views/index/public/foot.blade.php
@@ -0,0 +1,46 @@
+<div class="clear"> </div>
+<!---start-footer---->
+<div class="footer">
+    <div class="wrap">
+        <div class="footer-grids">
+            <div class="footer-grid">
+                <h3>EXTRAS</h3>
+                <p>Ut rutrum neque a mollis laoreet diam enim feuiat dui nec ulacoper quam felis id diam. Nunc ut tortor ligula eu petiu risus. Pelleesque conquat dignissim lacus quis altrcies.</p>
+            </div>
+            <div class="footer-grid">
+                <h3>RECENT POSTS</h3>
+                <ul>
+                    <li><a href="#">Vestibulum felis</a></li>
+                    <li><a href="#">Mauris at tellus</a></li>
+                    <li><a href="#">Donec ut lectus</a></li>
+                    <li><a href="#">vitae interdum</a></li>
+                </ul>
+            </div>
+            <div class="footer-grid">
+                <h3>USEFUL INFO</h3>
+                <ul>
+                    <li><a href="#">Hendrerit quam</a></li>
+                    <li><a href="#">Amet consectetur </a></li>
+                    <li><a href="#">Iquam hendrerit</a></li>
+                    <li><a href="#">Donec ut lectus </a></li>
+                </ul>
+            </div>
+            <div class="footer-grid footer-lastgrid">
+                <h3>CONTACT US</h3>
+                <p>Pelleesque conquat dignissim lacus quis altrcies.</p>
+                <div class="footer-grid-address">
+                    <p>Tel.800-255-9999</p>
+                    <p>Fax: 1234 568</p>
+                    <p>Email:<a class="email-link" href="#">info(at)yourcompany.com</a></p>
+                </div>
+            </div>
+            <div class="clear"> </div>
+        </div>
+    </div>
+</div>
+<!---End-footer---->
+<div class="clear"> </div>
+<div class="copy-right">
+    <p>Design Collect from <a href="http://www.cssmoban.com/" target="_blank" title="模板之家">模板之家</a></p>
+</div>
+</body>
---
+++ mengma/resources/views/index/public/style.blade.php
@@ -0,0 +1,16 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link href="css/style.css" rel="stylesheet" type="text/css"  media="all" />
+<link rel="stylesheet" href="css/responsiveslides.css">
+<script src="js/jquery.min.js"></script>
+<script src="js/responsiveslides.min.js"></script>
+<script>
+    // You can also use "$(window).load(function() {"
+    $(function () {
+        // Slideshow 1
+        $("#slider1").responsiveSlides({
+            maxwidth: 2500,
+            speed: 600
+        });
+    });
+</script>
